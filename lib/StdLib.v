22b2d63cfa0a509807bbd2e7a2de7f015762594f
josh <joshcohen98@gmail.com>
2025-04-20 19:51:45 -0400

    Fix proof system for InductionTest
    Rewrite bunch with lists, prove equivalence
    Need to move definitions, organize, replace tactics, push through

--- lib/StdLib.v
+++ lib/StdLib.v
@@ -16,7 +16,7 @@ Definition rec_fun f args body : def := recursive_def [fun_def f args body].
 Definition rec_pred p args body : def := recursive_def [pred_def p args body].
 
 Notation mk_typemap l := (exist _ l erefl).
-Definition emp_typemap : ty_map := (exist _ nil erefl).
+Definition emp_typemap : ty_map := (exist _ amap_empty erefl).
 
 Open Scope string_scope.
 Open Scope why3_scope.
